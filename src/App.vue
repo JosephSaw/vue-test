f81d07b837cca717fba34ff3179dad757327c8a0
theblockchaindeveloper <80172106+theblockchaindeveloper@users.noreply.github.com>
2021-06-29 18:19:14 +0800

    test set up

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <button @click="checkEthereum">Click me!</button>
+    <button @click="connetWallet">Connect Wallet!</button>
+
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -9,18 +11,60 @@
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import VConsole from "vconsole";
+import Onboard from "bnc-onboard";
+import Web3 from "web3";
+import ethers from "ethers"
 
 export default {
   name: "App",
   components: {
     HelloWorld,
   },
-  mounted(){
+  mounted() {
     new VConsole();
   },
   methods: {
     checkEthereum() {
       console.log("window.ethereum", window.ethereum);
+      console.log('ethers', ethers.utils.parseEther('1'))
+    },
+    connetWallet() {
+      // set a variable to store instantiated web3
+      let web3;
+
+      // head to blocknative.com to create a key
+      const BLOCKNATIVE_KEY = "blocknative-api-key";
+
+      // the network id that your dapp runs on
+      const NETWORK_ID = 1;
+
+      // initialize onboard
+      const onboard = Onboard({
+        dappId: BLOCKNATIVE_KEY,
+        networkId: NETWORK_ID,
+        subscriptions: {
+          wallet: (wallet) => {
+            // instantiate web3 when the user has selected a wallet
+            web3 = new Web3(wallet.provider);
+            console.log(`${wallet.name} connected!`);
+          },
+        },
+      });
+
+      // Prompt user to select a wallet
+      onboard
+        .walletSelect()
+        .then(() => {
+          // Run wallet checks to make sure that user is ready to transact
+
+          return onboard.walletCheck();
+        })
+        .then(() => {
+          console.log("wallet connected");
+        })
+        .catch((e) => {
+          console.error("");
+        });
     },
   },
 };
